dde02249f2422ad3c05c31b272619b032b9bedaf
Gonzalo Odiard <godiard@gmail.com>
2011-12-09 17:48:32 -0300

    Change WIKIDB parameter to use new data

--- activity_es.py
+++ activity_es.py
@@ -4,7 +4,7 @@ import activity
 class WikipediaActivityES(activity.WikipediaActivity):
 
     def __init__(self, handle):
-        self.WIKIDB = 'es_PE/es_PE.xml.bz2'
+        self.WIKIDB = 'es_new/eswiki-20111112-pages-articles.xml'
         self.HOME_PAGE = '/static/index_es.html'
         self.HTTP_PORT = '8000'
         activity.WikipediaActivity.__init__(self, handle)
