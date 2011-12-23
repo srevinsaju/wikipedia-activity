1e8e9d5bf2127b1a7d8308df3472518a59de07f3
Gonzalo Odiard <godiard@gmail.com>
2011-12-23 00:21:57 -0300

    Add a parameter to download_images.py to use a directory as cache

--- tools2/download_images.py
+++ tools2/download_images.py
@@ -8,7 +8,9 @@ import md5
 import urllib
 import os
 import sys
+import shutil
 
+import config
 
 class FileListReader():
 
@@ -23,8 +25,9 @@ class FileListReader():
 
 class ImagesDownloader:
 
-    def __init__(self, file_name, pages_selected, base_dir):
+    def __init__(self, file_name, pages_selected, base_dir, cache_dir):
         self.base_dir = base_dir
+        self.cache_dir = cache_dir
         self.templates_to_counter = {}
         input_links = open('%s.page_images' % file_name, mode='r')
         line = input_links.readline()
@@ -52,23 +55,37 @@ class ImagesDownloader:
             pass  # This just means that destdir already exists
         dest = "%s/%s" % (destdir, image_name)
         if not os.path.exists(dest):
+            if self.cache_dir is not None:
+                # Verify if the file is in the cahce_dir
+                cache_file = "%s/%s/%s/%s" % (self.cache_dir, dirs[0], dirs[1],
+                        image_name)
+                if os.path.exists(cache_file):
+                    shutil.copyfile(cache_file, dest)
+                    return
             print "Downloading %s" % url
             urllib.urlretrieve(url, dest)
 
 downlad_all = False
+cache_dir = None
 print sys.argv
 if len(sys.argv) > 1:
-    downlad_all = (sys.argv[1] == '--all')
-    print "Downloading all images"
+    for argn in range(1, len(sys.argv)):
+        arg = sys.argv[argn]
+        if arg == '--all':
+            downlad_all = True
+            print "Downloading all images"
+        if arg.startswith('--cache_dir='):
+            cache_dir = arg[arg.find('=') + 1:]
+            print "Using cache directory", cache_dir
 
-input_xml_file_name = './eswiki-20111112-pages-articles.xml'
+input_xml_file_name = config.input_xml_file_name
 
 selected_pages = None
 if not downlad_all:
     print "Loading selected pages"
-    favorites_reader = FileListReader("favorites.txt")
+    favorites_reader = FileListReader(config.favorites_file_name)
     selected_pages = favorites_reader.list
 
 print "Downloading images"
 templates_counter = ImagesDownloader(input_xml_file_name,
-        selected_pages, "./images")
+        selected_pages, "./images", cache_dir)
