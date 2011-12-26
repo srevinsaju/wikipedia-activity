322ba2be69b887cfb644fbf624810a4342c1e0d1
Gonzalo Odiard <godiard@gmail.com>
2011-12-26 15:55:22 -0300

    Change config in activity_es to use new generated data

--- activity_es.py
+++ activity_es.py
@@ -5,7 +5,7 @@ import activity
 class WikipediaActivityES(activity.WikipediaActivity):
 
     def __init__(self, handle):
-        self.WIKIDB = 'es_new/eswiki-20111112-pages-articles.xml'
+        self.WIKIDB = 'es_lat/eswiki-20111112-pages-articles.xml'
         self.HOME_PAGE = '/static/index_es.html'
         self.HTTP_PORT = '8000'
         activity.WikipediaActivity.__init__(self, handle)
