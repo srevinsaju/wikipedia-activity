a5193d22f3dc80b9c9c30b1ad70529492a5997c2
Gonzalo Odiard <godiard@gmail.com>
2012-01-15 23:39:46 -0300

    Updtave version in activity.info es and pl to 33.1

--- activity/activity.info.pl
+++ activity/activity.info.pl
@@ -1,6 +1,6 @@
 [Activity]
 name = WikipediaPL
-activity_version = 33
+activity_version = 33.1
 bundle_id = org.laptop.WikipediaPLActivity
 icon = activity-wikipedia-pl
 exec = sugar-activity activity_pl.WikipediaActivityPL
